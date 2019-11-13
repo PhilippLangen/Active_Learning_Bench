284d7ec62c552a0f0ef199c9de08c44d69046db2
Jan Philipp Simon Langen <jan_philipp_simon.langen@tu-dresden.de>
2019-11-13 19:03:23 +0100

    using new kcenter implementation

--- active_learning_benchmark.py
+++ active_learning_benchmark.py
@@ -10,7 +10,6 @@ import torch.nn as nn
 import torch.nn.functional as F
 import torchvision
 import torchvision.transforms as transforms
-from scipy import spatial
 from sklearn.decomposition import PCA
 
 
@@ -174,25 +173,22 @@ class ActiveLearningBench:
         confusion_matrix /= len(self.test_set.data)
         return correct/total, confusion_matrix.tolist()
 
-    def greedy_k_center(self, activations):                                             # TODO use torch.cdist instead
+    def greedy_k_center(self, activations):
         """
-        greedy k center strategy
-        :param activations:
-        :return:
-        """
-        activations = activations.to("cpu").numpy()
+                greedy k center strategy
+                :param activations:
+                :return:
+                """
         # unlabelled_data , labelled_data are in modified index order
         unlabelled_data = activations[self.unlabelled_idx]
         labelled_data = activations[self.labelled_idx]
-
         # create distance matrix axis 0 is unlabelled data 1 is labelled data
-        distance_matrix = spatial.distance.cdist(unlabelled_data, labelled_data)
+        distance_matrix = torch.cdist(unlabelled_data, labelled_data)
         # for each unlabelled date find min distance to labelled
-        min_dist = np.min(distance_matrix, axis=1)
-
+        min_dist, _ = torch.min(distance_matrix, dim=1)
         for i in range(self.budget):
             # find index of largest min_dist entry
-            idx = np.argmax(min_dist)
+            idx = torch.argmax(min_dist).to("cpu").item()
             # min_dist is in index modified order, so to find original order index use index list
             new_sample_idx = self.unlabelled_idx[idx]
             # get data of new labelled sample
@@ -202,21 +198,20 @@ class ActiveLearningBench:
             # add the original index of the new sample to the labelled indices
             self.labelled_idx = np.append(self.labelled_idx, new_sample_idx)
             # we also delete the data row, this makes same change to modified idx order balancing out
-            unlabelled_data = np.delete(unlabelled_data, idx, axis=0)
+            unlabelled_data = torch.cat((unlabelled_data[0:idx], unlabelled_data[idx + 1:]), dim=0)
             # Finally delete out of distance list, same change to mod idx order
-            min_dist = np.delete(min_dist, idx, axis=0)
-
+            min_dist = torch.cat((min_dist[0:idx], min_dist[idx + 1:]), dim=0)
             # now we need to see if sampling has minimized any min distances to labelled samples
             # finally calc min over 2 values
-            min_dist = \
-                np.min(
+            min_dist, _ = \
+                torch.min(
                     # add distances to new sample to old min distance_matrix values shape(x,1) -> (x,2)
-                    np.append(
-                        np.reshape(min_dist, (-1, 1)),
+                    torch.cat((
+                        torch.reshape(min_dist, (-1, 1)),
                         # first calc distance from unlabelled to new sample
-                        spatial.distance.cdist(unlabelled_data, np.reshape(new_sample_data, (1, -1))),
-                        axis=1),
-                    axis=1)
+                        torch.cdist(unlabelled_data, torch.reshape(new_sample_data, (1, -1)))),
+                        dim=1),
+                    dim=1)
 
         # update data loader
         labelled_sampler = torch.utils.data.SubsetRandomSampler(self.labelled_idx)
